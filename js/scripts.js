c43e0ea7e18b50563f6abaf476c37197e45d5839
tlpackus <tlpackus@gmail.com>
2021-01-25 13:57:23 -0800

    start totally new prac problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
   });
 });
